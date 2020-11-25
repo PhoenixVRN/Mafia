1c557d86c816ec398377f5e3f9f9bb3ef3bdf411
EvgenyiVRN <projectfenixru@gmail.com>
2020-11-25 13:21:04 +0300

    ЛогСкрин 25.11-1

--- app/src/main/java/com/fenix/app/ScrActivity.java
+++ app/src/main/java/com/fenix/app/ScrActivity.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class ScrActivity extends AppCompatActivity {
     private Button remuv;
+    private Button btnSignIn, btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
     private final View.OnClickListener remove_Listener = v -> {
-    this.finish();
- //       Intent intent = new Intent(MapsActivity.this, ScrActivity.class);
-  //      startActivity(intent);
+        this.finish();
+        //       Intent intent = new Intent(MapsActivity.this, ScrActivity.class);
+        //      startActivity(intent);
     };
 
     @Override
@@ -23,6 +30,12 @@ public class ScrActivity extends AppCompatActivity {
         remuv = (Button) findViewById(R.id.button_close);
         remuv.setOnClickListener(remove_Listener);
 
+        btnSignIn = findViewById(R.id.butSignIn);
+        btnRegister = findViewById(R.id.butRegister);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+        
 
     }
 }
