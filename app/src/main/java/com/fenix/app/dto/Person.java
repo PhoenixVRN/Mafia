f5104b63fe51e6dd67bc213002daf6017ada2525
Evgen <ekspert-ocenka@mail.ru>
2020-12-22 20:07:34 +0300

    22.12

--- app/src/main/java/com/fenix/app/dto/Person.java
+++ app/src/main/java/com/fenix/app/dto/Person.java
@@ -1,4 +1,6 @@
 package com.fenix.app.dto;
+import com.google.android.gms.maps.model.LatLng;
+
 import org.jetbrains.annotations.NotNull;
 
 import lombok.Data;
@@ -9,4 +11,29 @@ import lombok.NoArgsConstructor;
 
 
 public class Person {
+    private String namePerson;
+    private String idPerson;
+    private String classPerson;
+    private String rang;
+    private String bio;
+
+    private int streng;
+    private int agility;
+    private int mana;
+
+    private LatLng location = null;
+
+    Armor armorHead = new Armor();
+    Armor armorTorso = new Armor();
+    Armor armorLegs = new Armor();
+    Armor armorBoots = new Armor();
+    Armor armorGloves = new Armor();
+
+    Weapon weaponHeadLeft = new Weapon();
+    Weapon weaponHeadRight = new Weapon();
+
+
+
+
+
 }
