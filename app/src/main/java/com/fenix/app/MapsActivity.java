ec37099b9c1048de4b051b17ac80763d06d60c3a
EvgenyiVRN <projectfenixru@gmail.com>
2020-12-29 20:12:21 +0300

    29,12

--- app/src/main/java/com/fenix/app/MapsActivity.java
+++ app/src/main/java/com/fenix/app/MapsActivity.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -19,6 +20,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 
@@ -48,8 +52,9 @@ import org.bson.Document;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.SneakyThrows;
 import lombok.var;
+
+
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MapsActivity extends AppCompatActivity implements
@@ -141,6 +146,24 @@ public class MapsActivity extends AppCompatActivity implements
     };
     //#endregion
 
+    //# Вызов окна персонажа
+    private ImageButton iconPersBatton;
+    private View.OnContextClickListener iconPersBattonListener = new View.OnContextClickListener() {
+        @Override
+        public boolean onContextClick(View view) {
+//            var fragment = null;
+            var fragment = new PersonWindow();
+            var fm = getFragmentManager();
+            var ft = fm.beginTransaction();
+            ft.replace(R.id.map, fragment);
+            ft.commit();
+
+
+            return false;
+        }
+    };
+    //# end
+
     //#region mySwitch
     private Switch mySwitch;
     private CompoundButton.OnCheckedChangeListener mySwitchListener = (compoundButton, b) -> targetFollow = b;
@@ -217,6 +240,10 @@ public class MapsActivity extends AppCompatActivity implements
                     // myAreaButton
                     myAreaButton = (Button) findViewById(R.id.myAreaButton);
                     myAreaButton.setOnClickListener(myAreaButtonListener);
+
+                    // my iconPersBatton
+                    iconPersBatton = (ImageButton) findViewById(R.id.iconPersBatton);
+                    iconPersBatton.setOnContextClickListener(iconPersBattonListener);
 
                     // aliensSpinner
                     aliensSpinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item);
